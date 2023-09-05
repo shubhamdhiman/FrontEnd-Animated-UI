576df926ca488d0f0f4999143ec6051b4fa4a717
Shubham <shubhamdhiman00.sd@gmail.com>
2023-09-06 02:52:58 +0530

    note, everwondered, workwithus done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import EverWondered from './Components/EverWondered';
 import LandingPage from './Components/LandingPage';
 import MeetAhead from './Components/MeetAhead';
 import Navbar from './Components/Navbar';
+import Note from './Components/Note';
+import OpenVacancies from './Components/OpenVacancies';
 import SoundFamiliar from './Components/SoundFamiliar';
 import StaticContainer from './Components/StaticContainer';
-import { staticContainer1 ,staticContainer2} from './Utils';
+import Workwithus from './Components/Workwithus';
+import { staticContainer1 ,staticContainer2, workwithusData} from './Utils';
 function App() {
   return (
     <div className="App">
@@ -15,6 +19,11 @@ function App() {
       <SoundFamiliar />
       <MeetAhead />
       <StaticContainer colData={staticContainer2}/>
+      <EverWondered />
+      <Note />
+      <Workwithus data={workwithusData}/>
+      <OpenVacancies />
+      <hr className='w-full bg-gray-400'/>
     </div>
   );
 }
